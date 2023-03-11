878c4aeb3417e70b9ff96a6679fa8c368cc760d5
Emir Boyaci <emir@emirboyaci.dev>
2023-03-12 00:13:11 +0300

    feat: prevent safari ios auto zoom in input element

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,11 @@ import { ReactNode } from "react";
 export const metadata = {
   title: "Spotify Playlist to List",
   description: "Generate/export Spotify playlists as text and image just in a seconds!",
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
